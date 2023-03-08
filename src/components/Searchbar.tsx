89dd00dc48109eba3ffd134ad4fd581c389004b5
Horacio <horacio.schumann@gmail.com>
2023-03-08 16:59:18 -0300

    feat: criação da searchbar

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,34 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "../styles/Searchbar.css";
+
+export const Searchbar = () => {
+  const navigate = useNavigate();
+  const { searchTerm } = useParams();
+  const [term, setTerm] = useState(searchTerm || "");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  const handleClick = () => {
+    navigate(`pokemon/${term}`);
+  };
+  return (
+    <div className="search-bar">
+      <div className="search-input">
+        <input
+          type="text"
+          name="search"
+          value={term}
+          onChange={handleInput}
+          placeholder="Buscar pokemons..."
+        />
+        <button className="search-btn" onClick={handleClick}>
+          <FaSearch />
+        </button>
+      </div>
+    </div>
+  );
+};
