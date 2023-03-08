dd908184122de3d4ed6ddb59e1d58fd8d347f95d
Horacio <horacio.schumann@gmail.com>
2023-03-08 16:57:41 -0300

    feat: criação das rotas iniciais.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import Home from "./pages/Home";
+import App from "./App";
+import PokemonSearch from "./pages/PokemonSearch";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "pokemon/",
+        element: <PokemonSearch />,
+      },
+      {
+        path: "pokemon/:term",
+        element: <PokemonSearch />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
