0041160b11f4e609e1785e32415df805debe91b6
Horacio <horacio.schumann@gmail.com>
2023-03-13 17:59:04 -0300

    feat: adicionado o footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Outlet />
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
